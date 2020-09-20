d0f4b7c99bfe75743a1971a9b2b0449978b784e7
TejasWakalkar315 <tejaswakalkar315@gmail.com>
2020-09-20 19:07:35 +0530

    Section 4

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -12,6 +12,7 @@ namespace Persistence
         }
 
         public DbSet<Value> Values { get; set; }
+        public DbSet<Activity> Actitvities {get; set;}
 
         protected override void OnModelCreating(ModelBuilder builder){
             builder.Entity<Value>().HasData(
